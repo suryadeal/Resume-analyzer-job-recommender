d1e355353d936086b8b5238a87d1b07523d00f25
surya <suryadeal3@gmail.com>
2025-07-06 18:20:03 +0530

    updated

--- utils/parser.py
+++ utils/parser.py
@@ -2,7 +2,15 @@ import spacy
 from spacy.matcher import PhraseMatcher
 
 # Load English NLP model
-nlp = spacy.load("en_core_web_sm")
+import spacy
+from spacy.cli import download
+
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
 
 # Define known skills (you can expand this list)
 SKILLS = [
